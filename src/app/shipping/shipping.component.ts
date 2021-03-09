b1aa8dbb92b1e201f51f7316a6eee5e74a54275e
Alessandro Ferrero <ale.ferrero@icloud.com>
2021-03-09 12:43:53 +0100

    Fine progetto 1

---
+++ src/app/shipping/shipping.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { Price } from '../models/price';
+import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.css']
+})
+export class ShippingComponent implements OnInit {
+  shippingCosts: Price[] = [];
+
+  constructor(
+    private cartService: CartService
+  ) { }
+
+  ngOnInit(): void {
+    let observable: Observable<Price[]> = this.cartService.getShippingPrices();
+    observable.pipe(delay(5000)).subscribe(response => this.shippingCosts = response);
+  }
+
+}
